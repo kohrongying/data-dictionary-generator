dfeafdf547041fdc3c4edd33d3ef6e99e6fdaa39
rongying <rongying.koh@thoughtworks.com>
2022-04-05 16:19:39 +0800

    [rongying] refactor psql script name.

--- run.sh
+++ run.sh
@@ -18,13 +18,15 @@ get_tables() {
 }
 
 generate_columns() {
-    sed 's/$TABLE_NAME/'"$TABLE_NAME"'/g;s/$SCHEMA_NAME/'"$SCHEMA_NAME"'/g'  scripts/get_columns.template.sql > scripts/get_columns.sql
-    docker exec $POSTGRES_CONTAINER psql -t -U postgres -d postgres -f './tmp/get_columns.sql' | awk -F"|" '$1!=""{print "{\"column_name\": \""$1"\", \"data_type\": \""$2"\", \"primary\": \"\", \"nullable\": \""$3"\", \"description\": \"\", \"identity\": \""$4"\", \"character_max_length\": \""$5"\", \"numeric_precision\": \""$6"\", \"numeric_scale\": \""$7"\", \"numeric_precision_radix\": \""$8"\" }" }' >> "$COLUMN_TMP_FILE"
+    SCRIPT_NAME="get_columns"
+    sed 's/$TABLE_NAME/'"$TABLE_NAME"'/g;s/$SCHEMA_NAME/'"$SCHEMA_NAME"'/g'  scripts/$SCRIPT_NAME.template.sql > scripts/$SCRIPT_NAME.sql
+    docker exec $POSTGRES_CONTAINER psql -t -U postgres -d postgres -f "./tmp/$SCRIPT_NAME.sql" | awk -F"|" '$1!=""{print "{\"column_name\": \""$1"\", \"data_type\": \""$2"\", \"primary\": \"\", \"nullable\": \""$3"\", \"description\": \"\", \"identity\": \""$4"\", \"character_max_length\": \""$5"\", \"numeric_precision\": \""$6"\", \"numeric_scale\": \""$7"\", \"numeric_precision_radix\": \""$8"\" }" }' >> "$COLUMN_TMP_FILE"
 }
 
 generate_indexes() {
-    sed 's/$TABLE_NAME/'"$TABLE_NAME"'/g;' scripts/get_indexes.template.sql > scripts/get_indexes.sql
-    docker exec $POSTGRES_CONTAINER psql -t -U postgres -d postgres -f './tmp/get_indexes.sql' | xargs | tr " " ","
+    SCRIPT_NAME="get_indexes"
+    sed 's/$TABLE_NAME/'"$TABLE_NAME"'/g;' scripts/$SCRIPT_NAME.template.sql > scripts/$SCRIPT_NAME.sql
+    docker exec $POSTGRES_CONTAINER psql -t -U postgres -d postgres -f "./tmp/$SCRIPT_NAME.sql" | xargs | tr " " ","
 }
 
 generate_table() {

2b166a7ac4a99192354faa8a292b03195a768f3f
rongying <rongying.koh@thoughtworks.com>
2022-04-05 16:14:31 +0800

    [rongying] add indexes query.

--- run.sh
+++ run.sh
@@ -22,12 +22,21 @@ generate_columns() {
     docker exec $POSTGRES_CONTAINER psql -t -U postgres -d postgres -f './tmp/get_columns.sql' | awk -F"|" '$1!=""{print "{\"column_name\": \""$1"\", \"data_type\": \""$2"\", \"primary\": \"\", \"nullable\": \""$3"\", \"description\": \"\", \"identity\": \""$4"\", \"character_max_length\": \""$5"\", \"numeric_precision\": \""$6"\", \"numeric_scale\": \""$7"\", \"numeric_precision_radix\": \""$8"\" }" }' >> "$COLUMN_TMP_FILE"
 }
 
+generate_indexes() {
+    sed 's/$TABLE_NAME/'"$TABLE_NAME"'/g;' scripts/get_indexes.template.sql > scripts/get_indexes.sql
+    docker exec $POSTGRES_CONTAINER psql -t -U postgres -d postgres -f './tmp/get_indexes.sql' | xargs | tr " " ","
+}
+
 generate_table() {
+    # add comma to end of each line except last line (jsonl -> json)
     COLUMNS=$(sed -e "$ ! s/$/,/" "$COLUMN_TMP_FILE")
+    
+    INDEXES=$(generate_indexes)
 
     jq --null-input \
     --arg TABLE_NAME "$TABLE_NAME" \
     --arg SCHEMA_NAME "$SCHEMA_NAME" \
+    --arg INDEXES "$INDEXES" \
     --argjson COLUMNS "[$COLUMNS]" \
     '$ARGS.named' > $OUTPUT_FOLDER/$TABLE_NAME.json
 

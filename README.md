c17799bfbb8ae05a1ec40a53ec803235f74526a3
rongying <rongying.koh@thoughtworks.com>
2022-04-05 16:44:28 +0800

    [rongying] update pdf generation to include indexes pri keys and other fields.

--- README.md
+++ README.md
@@ -11,7 +11,16 @@ Generating data dictionary from scratch
 ./init.sh
 ```
 
-# Enhancements
-1. combine table json output for table
+# Interesting 
+`psql -t` means to pset tuples_only (no header and no footer)
+`psql -c` to run one psql command
+`psql -f` to run one psql file
 
+# Enhancements
+[x] specify database and schema
+[x] get index
+[x] get primary key
+
+1. combine table json output for table
+2. get foreign key
 
--- src/makepdf.js
+++ src/makepdf.js
@@ -4,32 +4,60 @@ const path = require('path');
 
 const buildTable = (data) => {
     const table_name = data.TABLE_NAME;
+    const schema_name = data.SCHEMA_NAME;
+    
+    return {
+        stack: [
+            { text: `Project: ${constants.PROJECT_NAME}` },
+            { text: `Schema: ${schema_name}` },
+            { text: `Table: ${table_name}`, margin: [0, 0, 0, 10] },
+            buildIndexesTable(data),
+            buildColumnsTable(data)
+        ]
+    }
+}
+
+const buildIndexesTable = (data) => {
+    const indexes =  data.INDEXES.replace(",","\n")
+    const body = [
+        ['Index', indexes]
+    ]
+    const widths = [200, '*']
+    return { 
+        table: { 
+            widths,
+            body, 
+        },
+        margin: [0, 0, 0, 10]
+    }
+}
+
+const buildColumnsTable = (data) => {
+    const PKEYS = data.PKEYS.split(",")
     const HEADERS = [
-        'S/N', 'PK', 'Column Name', 'Data Type', 'Nullable', 'Description'
+        'S/N', 'PK', 'Column Name', 'Data Type', 'Max Length', 'Nullable', 'Description'
     ]
     const widths = [
-        50, 50, '*', '*', 200, '*'
+        50, 100, '*', '*', 200, 100, '*'
     ]
     const body = [HEADERS]
     data.COLUMNS.forEach((column, index) => {
         body.push([
-            `${index+1}`, "", column.column_name, column.data_type, column.nullable, column.description
+            `${index+1}`, 
+            PKEYS.includes(column.column_name.trim()) ? "Y" : "", 
+            column.column_name, 
+            column.data_type,
+            column.character_max_length, 
+            column.nullable,
+            column.description
         ])
     })
-
-    return {
-        stack: [
-            { text: `Project: ${constants.PROJECT_NAME}` },
-            { text: `Database: Feedback` },
-            { text: `Table: ${table_name}`, margin: [0, 0, 0, 10] },
-            { 
-                table: { 
-                    widths,
-                    body, 
-                },
-                margin: [0, 0, 0, 20]
-            }
-        ]
+    return { 
+        table: { 
+            widths,
+            body, 
+        },
+        margin: [0, 0, 0, 20]
     }
 }
 
